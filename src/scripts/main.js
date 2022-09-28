6b9bd440801306a02b8d01226d685211e3194485
Usman Husnan <oets.man@gmail.com>
2022-09-29 01:45:46 +0700

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,114 @@
-function main() {
+import { async } from "regenerator-runtime";
 
+function main() {
+  const baseUrl = "https://books-api.dicoding.dev";
+  const getBook_xhr = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      const responseJSON = JSON.parse(this.responseText);
+      if (responseJSON.error) {
+        showResponseMessage(responseJSON.message);
+      } else {
+        renderAllBooks(responseJSON.books);
+      }
+    };
+    xhr.onerror = function () {
+      showResponseMessage();
+    };
+    xhr.open("GET", `${baseUrl}/list`);
+    xhr.send();
+  };
   const getBook = () => {
-    // tuliskan kode di sini!
+    fetch(`${baseUrl}/list`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJSON) => {
+        if (responseJSON.error) {
+          showResponseMessage(this.message);
+        } else {
+          renderAllBooks(responseJSON.books);
+        }
+      })
+      .catch((error) => {
+        showResponseMessage(error);
+      });
   };
 
-
-  const insertBook = (book) => {
-    // tuliskan kode di sini!
+  const insertBook_xhr = (book) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      const responseJSON = JSON.parse(this.responseText);
+      showResponseMessage(responseJSON.message);
+      getBook();
+    };
+    xhr.onerror = function () {
+      showResponseMessage();
+    };
+    xhr.open("POST", `${baseUrl}/add`);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-Auth-Token", "12345");
+    xhr.send(JSON.stringify(book));
+  };
+  const insertBook = async (book) => {
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Auth-Token": "12345",
+        },
+        body: JSON.stringify(book),
+      };
+      const response = await fetch(`${baseUrl}/add`, options);
+      const responseJson = await response.json();
+      showResponseMessage(responseJson.message);
+      getBook();
+    } catch (error) {
+      showResponseMessage(error);
+    }
   };
 
   const updateBook = (book) => {
-    // tuliskan kode di sini!
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      const responseJSON = JSON.parse(this.responseText);
+      showResponseMessage(responseJSON.message);
+      getBook();
+    };
+    xhr.onerror = function () {
+      showResponseMessage();
+    };
+    xhr.open("PUT", `${baseUrl}/edit/${book.id}`);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("X-Auth-Token", "12345");
+    xhr.send(JSON.stringify(book));
   };
 
   const removeBook = (bookId) => {
-    // tuliskan kode di sini!
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      const responseJSON = JSON.parse(this.responseText);
+      showResponseMessage(responseJSON.message);
+      getBook();
+    };
+    xhr.onerror = function () {
+      showResponseMessage();
+    };
+    xhr.open("DELETE", `${baseUrl}/delete/${bookId}`);
+    xhr.setRequestHeader("X-Auth-Token", "12345");
+    xhr.send();
   };
 
-
-  
-  
-  
-  
   /*
       jangan ubah kode di bawah ini ya!
   */
 
   const renderAllBooks = (books) => {
-    const listBookElement = document.querySelector('#listBook');
-    listBookElement.innerHTML = '';
+    const listBookElement = document.querySelector("#listBook");
+    listBookElement.innerHTML = "";
 
-    books.forEach(book => {
+    books.forEach((book) => {
       listBookElement.innerHTML += `
         <div class="col-lg-4 col-md-6 col-sm-12" style="margin-top: 12px;">
           <div class="card">
@@ -44,43 +122,42 @@ function main() {
       `;
     });
 
-    const buttons = document.querySelectorAll('.button-delete');
-    buttons.forEach(button => {
-      button.addEventListener('click', event => {
+    const buttons = document.querySelectorAll(".button-delete");
+    buttons.forEach((button) => {
+      button.addEventListener("click", (event) => {
         const bookId = event.target.id;
-        
+
         removeBook(bookId);
       });
     });
   };
 
-  const showResponseMessage = (message = 'Check your internet connection') => {
+  const showResponseMessage = (message = "Check your internet connection") => {
     alert(message);
   };
 
-  document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const inputBookId = document.querySelector("#inputBookId");
+    const inputBookTitle = document.querySelector("#inputBookTitle");
+    const inputBookAuthor = document.querySelector("#inputBookAuthor");
+    const buttonSave = document.querySelector("#buttonSave");
+    const buttonUpdate = document.querySelector("#buttonUpdate");
 
-    const inputBookId = document.querySelector('#inputBookId');
-    const inputBookTitle = document.querySelector('#inputBookTitle');
-    const inputBookAuthor = document.querySelector('#inputBookAuthor');
-    const buttonSave = document.querySelector('#buttonSave');
-    const buttonUpdate = document.querySelector('#buttonUpdate');
-
-    buttonSave.addEventListener('click', function () {
+    buttonSave.addEventListener("click", function () {
       const book = {
         id: Number.parseInt(inputBookId.value),
         title: inputBookTitle.value,
-        author: inputBookAuthor.value
+        author: inputBookAuthor.value,
       };
-      
+
       insertBook(book);
     });
 
-    buttonUpdate.addEventListener('click', function () {
+    buttonUpdate.addEventListener("click", function () {
       const book = {
         id: Number.parseInt(inputBookId.value),
         title: inputBookTitle.value,
-        author: inputBookAuthor.value
+        author: inputBookAuthor.value,
       };
 
       updateBook(book);
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,22 @@
-const {merge} = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
         use: [
           {
-            loader: 'babel-loader',
-            exclude: /(node_modules|bower_components)/,
+            loader: "babel-loader",
+            // exclude: /(node_modules|bower_components)/,
             options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        ]
-      }
-    ]
-  }
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
 });
